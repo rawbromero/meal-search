fad2b998b7579528c4b450c3939a79aef72f2b66
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-11-22 20:32:55 -0600

    homepage is working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,6 +5,7 @@ import Meals from '@/components/Meals.vue'
 
 const { api } = useAPI()
 const meals = ref([])
+
 onMounted(async () => {
     for (let index = 0; index < 9; index++) {
       api.get('random.php').then( ({data}) => meals.value.push(data.meals[0]))
@@ -12,18 +13,11 @@ onMounted(async () => {
     }
 })
 
-
-
-
 </script>
-
-
 
 <template>
   <div class="p-8 pb-0 text-orange-500">
     <h1 class="text-4xl font-bold mb-4"> Random Meals</h1>
   </div>
   <Meals :meals="meals"></Meals>
-
-  
 </template>
