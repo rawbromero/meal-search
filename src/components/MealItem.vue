9dccfe3336ecd4b5bae8e81c465b58c0257b5d06
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-11-22 20:16:11 -0600

    random check

---
+++ src/components/MealItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+const { meal } = defineProps({
+    meal: {
+        required: true,
+        type: Object
+    }
+})
+</script>
+
+<template>
+
+    <div class="bg-white shadow rounded-xl">
+        <router-link :to="{name: 'MealDetails', params: {id: meal.idMeal}}">
+            <img :src="meal.strMealThumb" class="rounded-t-xl h-48 object-cover w-full"/>
+        </router-link>
+        <div class="p-3">
+            <h3 class="font-bold">{{meal.strMealThumb  }}</h3>
+            <p class="mb-4 line-clamp-3">{{ meal.strInstructions }}</p> 
+        </div>
+    </div>
+    
+
+
+</template>
